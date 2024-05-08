fixed some scaling issues

draw_util.py
@@ -73,7 +73,7 @@ def draw_vec_field(
     i_scale = surface.get_width() / field.shape[0]
     j_scale = surface.get_height() / field.shape[1]
 
-    center_shfit = -pg.Vector2(i_scale / 2, j_scale / 2)
+    center_shfit = pg.Vector2(i_scale / 2, j_scale / 2)
 
     for i, j in np.ndindex(field.shape[:2]):
         di, dj = field[i, j]

fluid.py
@@ -86,6 +86,33 @@ def grid(arr: tf.Tensor, *tgt_shape: list[int]) -> tf.Tensor:
 
 
 @tf.function
+def induce(
+    field: tf.Tensor, val: tf.Tensor, depth: tf.Tensor, a: tf.Tensor
+) -> tf.Tensor:
+    inv_a = 1 - a
+
+    horz_side = tf.fill((tf.shape(field)[0], depth), val * a)
+    vert_side = tf.fill((depth, tf.shape(field)[1] - 2 * depth), val * a)
+    
+    return tf.concat(
+        [
+            horz_side + field[:, :depth] * inv_a,
+            tf.concat(
+                [
+                    vert_side + field[:depth, depth:-depth] * inv_a,
+                    field[depth:-depth, depth:-depth],
+                    vert_side + field[-depth:, depth:-depth] * inv_a,
+                ],
+                axis=0,
+            ),
+            horz_side + field[:, -depth:] * inv_a,
+        ],
+        axis=1,
+    )
+
+
+
+@tf.function
 def advect(
     field: tf.Tensor, vel_i: tf.Tensor, vel_j: tf.Tensor, dt: tf.Tensor
 ) -> tf.Tensor:
@@ -114,13 +141,19 @@ def advect(
         shape=field_shape,
     )
     fc = tf.scatter_nd(
-        tf.stack([f_i, c_j], axis=2), ((1 - frac_i) * frac_j) * field, shape=field_shape
+        tf.stack([f_i, c_j], axis=2),
+        ((1 - frac_i) * frac_j) * field,
+        shape=field_shape,
     )
     cf = tf.scatter_nd(
-        tf.stack([c_i, f_j], axis=2), (frac_i * (1 - frac_j)) * field, shape=field_shape
+        tf.stack([c_i, f_j], axis=2),
+        (frac_i * (1 - frac_j)) * field,
+        shape=field_shape,
     )
     cc = tf.scatter_nd(
-        tf.stack([c_i, c_j], axis=2), (frac_i * frac_j) * field, shape=field_shape
+        tf.stack([c_i, c_j], axis=2),
+        (frac_i * frac_j) * field,
+        shape=field_shape,
     )
 
     return ff + fc + cf + cc
@@ -162,7 +195,7 @@ def get_forces(
     rr_float = tf.cast(rr, dtype=FLOAT)
     cc_float = tf.cast(cc, dtype=FLOAT)
 
-    pos_i, pos_j = (pos_i - loc_i) / scale, (pos_j - loc_j) / scale
+    pos_i, pos_j = pos_i / scale - loc_i, pos_j / scale - loc_j
     vel_i, vel_j = vel_i / scale, vel_j / scale
     vel_i = -(cc_float - pos_i) * avel + vel_i
     vel_j = (rr_float - pos_j) * avel + vel_j
@@ -244,7 +277,7 @@ def shape_to_rrcc(
     l2w = obj.local_to_world
 
     def pm_to_grid(pos: pm.Vec2d) -> pm.Vec2d:
-        return (pos - loc) / scale
+        return pos / scale - loc
 
     if isinstance(shape, pm.Circle):
         rr, cc = draw.ellipse(
@@ -265,23 +298,64 @@ def shape_to_rrcc(
     return ctt(rr, dtype=INT), ctt(cc, dtype=INT)
 
 
+# @tf.function
+# def shift_old(inputs, shift, axes, pad_val=0):
+#     assert len(shift) == len(axes)
+#     axis_shift = zip(axes, shift)
+#     axis2shift = dict(axis_shift)
+#     old_shape = inputs.shape
+
+#     for axis in axis2shift:
+#         pad_shape = list(inputs.shape)
+#         pad_shape[axis] = abs(axis2shift[axis])
+#         input_pad = tf.fill(pad_shape, pad_val)
+#         inputs = tf.concat((inputs, input_pad), axis=axis)
+
+#     input_roll = tf.roll(inputs, shift, axes)
+#     ret = tf.slice(input_roll, [0 for _ in range(len(old_shape))], old_shape)
+
+#     return ret
+
+
 @tf.function
-def shift(inputs, shift, axes, pad_val=0):
-    assert len(shift) == len(axes)
-    axis_shift = zip(axes, shift)
-    axis2shift = dict(axis_shift)
-    old_shape = inputs.shape
+def shift(field: tf.Tensor, i: tf.Tensor, j: tf.Tensor, fill: tf.Tensor) -> tf.Tensor:
+    if i < 0:
+        i = tf.abs(i)
+        field = tf.concat(
+            [
+                field[i:, :],
+                tf.fill((i, tf.shape(field)[1]), fill),
+            ],
+            axis=0,
+        )
+    elif i > 0:
+        field = tf.concat(
+            [
+                tf.fill((i, tf.shape(field)[1]), fill),
+                field[:-i, :],
+            ],
+            axis=0,
+        )
 
-    for axis in axis2shift:
-        pad_shape = list(inputs.shape)
-        pad_shape[axis] = abs(axis2shift[axis])
-        input_pad = tf.fill(pad_shape, pad_val)
-        inputs = tf.concat((inputs, input_pad), axis=axis)
+    if j < 0:
+        j = tf.abs(j)
+        field = tf.concat(
+            [
+                field[:, j:],
+                tf.fill((tf.shape(field)[0], j), fill),
+            ],
+            axis=1,
+        )
+    elif j > 0:
+        field = tf.concat(
+            [
+                tf.fill((tf.shape(field)[0], j), fill),
+                field[:, :-j],
+            ],
+            axis=1,
+        )
 
-    input_roll = tf.roll(inputs, shift, axes)
-    ret = tf.slice(input_roll, [0 for _ in range(len(old_shape))], old_shape)
-
-    return ret
+    return field
 
 
 class Fluid:
@@ -304,6 +378,10 @@ class Fluid:
             tf.constant(np.full(shape, 1, dtype=np.float32), dtype=FLOAT)
         )
 
+        ind_mask = np.full(shape, 1.0, dtype=np.float32)
+        ind_mask[3: -3, 3: -3] = 0.0
+        self._ind_mask = ctt(ind_mask, dtype=FLOAT)
+
     def run(self, steps: int, dt: float = 0.1) -> None:
         vel_i, vel_j, dens = self.vel_i, self.vel_j, self.dens
 
@@ -319,9 +397,9 @@ class Fluid:
             self.vel_i, self.vel_j, self.dens, tf.constant(dt, dtype=FLOAT)
         )
 
-        self.vel_i.assign(vel_i)
-        self.vel_j.assign(vel_j)
-        self.dens.assign(dens)
+        self.vel_i.assign(induce(vel_i, ctt(self.current[0], dtype=FLOAT), ctt(3, dtype=INT), ctt(1.0, dtype=FLOAT)))
+        self.vel_j.assign(induce(vel_j, ctt(self.current[1], dtype=FLOAT), ctt(3, dtype=INT), ctt(1.0, dtype=FLOAT)))
+        self.dens.assign(induce(dens, ctt(1.0, dtype=FLOAT), ctt(3, dtype=INT), ctt(1.0, dtype=FLOAT)))
 
     def draw(self, surface: pg.Surface, num_arrows: int = 25) -> None:
         surface.fill([255, 255, 255])
@@ -347,16 +425,12 @@ class Fluid:
     # "move" this field to be centered around position
     # pos is in PM-coordinates
     def shift(self, i: int, j: int) -> None:
-        shifts = [i, j]
-        axes = [0, 1]
+        i = ctt(i, dtype=INT)
+        j = ctt(j, dtype=INT)
 
-        self.vel_i.assign(
-            shift(self.vel_i, shifts, axes, ctt(self.current.x, dtype=FLOAT))
-        )
-        self.vel_j.assign(
-            shift(self.vel_j, shifts, axes, ctt(self.current.y, dtype=FLOAT))
-        )
-        self.dens.assign(shift(self.dens, shifts, axes, ctt(1.0, dtype=FLOAT)))
+        self.vel_i.assign(shift(self.vel_i, i, j, ctt(self.current.x, dtype=FLOAT)))
+        self.vel_j.assign(shift(self.vel_j, i, j, ctt(self.current.y, dtype=FLOAT)))
+        self.dens.assign(shift(self.dens, i, j, ctt(1.0, dtype=FLOAT)))
 
         self.loc = self.loc[0] + i, self.loc[1] + j
 
@@ -388,10 +462,12 @@ class Fluid:
             ctt(self.loc[1], dtype=FLOAT),
         )
         force = pm.Vec2d(force_i.numpy(), force_j.numpy())
+        print(force, torque.numpy())
         obj.apply_force_at_world_point(force, obj.center_of_gravity)
         obj.torque += torque.numpy()
         self.vel_i.assign(vel_i)
         self.vel_j.assign(vel_j)
+
 
 
 if __name__ == "__main__":

hull.py
@@ -3,8 +3,8 @@ import pymunk as pm
 import pygame as pg
 
 import util
-import draw_util
 import fluid
+import draw_util
 
 
 def reflect(points: list[pm.Vec2d], scale: float) -> list[pm.Vec2d]:
@@ -132,24 +132,33 @@ class Water(pm.Space):
 
     def step(self, dt: float):
         # any way to multi-thread this bit here?
-        self.air.step(dt)
-        self.surf.step(dt)
-        self.deep.step(dt)
-        print(list(self.objects.items()))
-        for obj, data in self.objects.items():
-            for shapes, fld in zip(data, [self.air, self.surf, self.deep]):
-                for shape, weight in shapes:
-                    fld.apply_forces(obj, shape, weight)
 
         super().step(dt)
 
-        self.center()
+        self.air.step(dt)
+        self.surf.step(dt)
+        self.deep.step(dt)
+
+        for obj, data in self.objects.items():
+            for shape, weight in data[0]:
+                self.air.apply_forces(obj, shape, weight)
+
+            for shape, weight in data[1]:
+                self.surf.apply_forces(obj, shape, weight)
+
+            for shape, weight in data[2]:
+                self.deep.apply_forces(obj, shape, weight)
+
+        print(hull.torque, hull.force)
+
+        # self.center()
 
 
 if __name__ == "__main__":
-    space = Water((500, 500), scale=10.0, current=pm.Vec2d(0, -3))
+    space = Water((500, 500), scale=2.0, current=pm.Vec2d(0, 0), loc=(-250, -250))
     hull = pm.Body()
-    r, s = 3, 40
+
+    r, s = 25, 8
     hull_poly = [
         [r * np.cos(i * 2 * np.pi / s), r * np.sin(i * 2 * np.pi / s)] for i in range(s)
     ]
@@ -163,6 +172,7 @@ if __name__ == "__main__":
     # hull_poly = reflect(hull_poly, scale=space.scale)
 
     hull_shape = pm.Poly(hull, hull_poly)
+    hull_shape.density = 1.0
 
     space.add(hull, hull_shape)
 
@@ -174,7 +184,7 @@ if __name__ == "__main__":
     # rudder_hinge = Hinge(hull, rudder, rudder_a)
     space.add_obj(
         hull,
-        surf=[(hull_shape, 0.75)],
+        surf=[(hull_shape, 1.0)],
         # deep=[keel]
     )
     # space.add_obj(
@@ -191,6 +201,9 @@ if __name__ == "__main__":
 
     pg.init()
 
+    hull.angular_velocity = 1.0
+    hull.velocity = pm.Vec2d(10, 0)
+
     surface = pg.display.set_mode((1000, 1000))
 
     clock = pg.time.Clock()
@@ -199,26 +212,26 @@ if __name__ == "__main__":
     # space.surf.vel[...] = [5, 0] + util.RNG.uniform(-1, 1, space.surf.vel.shape)
 
     def draw():
-        shift = hull.position
-        scale = surface.get_width() / space.shape[0]
+        scale = surface.get_width() / (space.shape[0] * space.scale)
 
         def pm_to_pg(pos: pm.Vec2d) -> pm.Vec2d:
             return util.to_int(
-                (hull.local_to_world(pos) - shift) * scale
-                + pm.Vec2d(*surface.get_size()) * 0.5
+                (hull.local_to_world(pos) - pm.Vec2d(*space.loc) * space.scale) * scale
+                # + pm.Vec2d(*surface.get_size()) * 0.5
             )
 
         surface.fill([255, 255, 255])
 
-        space.surf.draw(surface, num_arrows=20)
+        space.surf.draw(surface, num_arrows=30)
 
         # draw surface water
-        draw_util.draw_field(surface, util.normalize(space.surf.dens), [-1, -1, 0])
+        # draw_util.draw_field(surface, util.normalize(space.surf.dens), [-1, -1, 0])
 
         # draw hull
-        pg.draw.aalines(
-            surface, [0, 0, 0], True, [pm_to_pg(vert) for vert in hull_poly]
-        )
+        draw_util.draw_shape(surface, [0, 0, 0], hull_shape, surface.get_width() / space.shape[0])
+        # pg.draw.aalines(
+        #     surface, [0, 0, 0], True, [pm_to_pg(vert) for vert in hull_poly]
+        # )
 
         # # draw rudder
         # pg.draw.aaline(
@@ -237,7 +250,8 @@ if __name__ == "__main__":
             if event.type == pg.QUIT:
                 running = False
 
-        space.step(0.6)
+        space.step(1 / 30)
+
         draw()
 
-        clock.tick(10)
+        # clock.tick(60)
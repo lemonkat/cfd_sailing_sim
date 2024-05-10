4dbdb8fd18cc7db0331fc1245a3eaa55bdbaccd0
Lemon Kat <lemonkatto@gmail.com>
2024-05-10 07:57:59 -0700

    readme update and moving some code around

--- fluid.py
+++ fluid.py
@@ -93,7 +93,7 @@ def induce(
 
     horz_side = tf.fill((tf.shape(field)[0], depth), val * a)
     vert_side = tf.fill((depth, tf.shape(field)[1] - 2 * depth), val * a)
-    
+
     return tf.concat(
         [
             horz_side + field[:, :depth] * inv_a,
@@ -109,7 +109,6 @@ def induce(
         ],
         axis=1,
     )
-
 
 
 @tf.function
@@ -220,7 +219,13 @@ def get_forces(
 
 @tf.function
 def step(
-    vel_i: tf.Tensor, vel_j: tf.Tensor, dens: tf.Tensor, dt: tf.Tensor
+    vel_i: tf.Tensor,
+    vel_j: tf.Tensor,
+    dens: tf.Tensor,
+    bg_vel_i: tf.Tensor,
+    bg_vel_j: tf.Tensor,
+    bg_dens: tf.Tensor,
+    dt: tf.Tensor,
 ) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
     # vel_i_1 = advect(vel_i, vel_i, vel_j, dt)
     # vel_j_1 = advect(vel_j, vel_i, vel_j, dt)
@@ -245,7 +250,11 @@ def step(
     vel_i = mtm_i_1 / dens + pv_i
     vel_j = mtm_j_1 / dens + pv_j
 
-    return vel_i, vel_j, dens
+    return (
+        induce(vel_i, bg_vel_i, 3.0, 1),
+        induce(vel_j, bg_vel_j, 3.0, 1),
+        induce(dens, bg_dens, 3.0, 1),
+    )
 
 
 # class StepModel(tf.keras.Model):
@@ -358,157 +367,185 @@ def shift(field: tf.Tensor, i: tf.Tensor, j: tf.Tensor, fill: tf.Tensor) -> tf.T
     return field
 
 
-class Fluid:
-    def __init__(
-        self,
-        shape: tuple[int, int],
-        current: pm.Vec2d,
-        loc: tuple[int, int] = (0, 0),
-        scale: float = 1.0,
-    ):
-        self.shape = shape
-        self.current = current
-        self.vel_i = tf.Variable(tf.cast(tf.fill(shape, current.x), dtype=FLOAT))
-        self.vel_j = tf.Variable(tf.cast(tf.fill(shape, current.y), dtype=FLOAT))
+# class Fluid:
+#     def __init__(
+#         self,
+#         shape: tuple[int, int],
+#         current: pm.Vec2d,
+#         loc: tuple[int, int] = (0, 0),
+#         scale: float = 1.0,
+#     ):
+#         self.shape = shape
+#         self.current = current
+#         self.vel_i = tf.Variable(tf.cast(tf.fill(shape, current.x), dtype=FLOAT))
+#         self.vel_j = tf.Variable(tf.cast(tf.fill(shape, current.y), dtype=FLOAT))
 
-        self.loc = loc  # in PM-coordinates, position of (0, 0)
-        self.scale = scale  # 1 grid = how many PM units?
+#         self.loc = loc  # in PM-coordinates, position of (0, 0)
+#         self.scale = scale  # 1 grid = how many PM units?
 
-        self.dens = tf.Variable(
-            tf.constant(np.full(shape, 1, dtype=np.float32), dtype=FLOAT)
-        )
+#         self.dens = tf.Variable(
+#             tf.constant(np.full(shape, 1, dtype=np.float32), dtype=FLOAT)
+#         )
 
-        ind_mask = np.full(shape, 1.0, dtype=np.float32)
-        ind_mask[3: -3, 3: -3] = 0.0
-        self._ind_mask = ctt(ind_mask, dtype=FLOAT)
+#         ind_mask = np.full(shape, 1.0, dtype=np.float32)
+#         ind_mask[3:-3, 3:-3] = 0.0
+#         self._ind_mask = ctt(ind_mask, dtype=FLOAT)
 
-    def run(self, steps: int, dt: float = 0.1) -> None:
-        vel_i, vel_j, dens = self.vel_i, self.vel_j, self.dens
+#     def run(self, steps: int, dt: float = 0.1) -> None:
+#         vel_i, vel_j, dens = self.vel_i, self.vel_j, self.dens
 
-        for _ in range(steps):
-            vel_i, vel_j, dens = step(vel_i, vel_j, dens, tf.constant(dt, dtype=FLOAT))
+#         for _ in range(steps):
+#             vel_i, vel_j, dens = step(vel_i, vel_j, dens, tf.constant(dt, dtype=FLOAT))
 
-        self.vel_i.assign(vel_i)
-        self.vel_j.assign(vel_j)
-        self.dens.assign(dens)
+#         self.vel_i.assign(vel_i)
+#         self.vel_j.assign(vel_j)
+#         self.dens.assign(dens)
 
-    def step(self, dt: float = 0.1) -> None:
-        vel_i, vel_j, dens = step(
-            self.vel_i, self.vel_j, self.dens, tf.constant(dt, dtype=FLOAT)
-        )
+#     def step(self, dt: float = 0.1) -> None:
+#         vel_i, vel_j, dens = step(
+#             self.vel_i,
+#             self.vel_j,
+#             self.dens,
+#             tf.constant(self.current.x, dtype=FLOAT),
+#             tf.constant(self.current.y, dtype=FLOAT),
+#             tf.constant(1.0, dtype=FLOAT),
+#             tf.constant(dt, dtype=FLOAT),
+#         )
+#         self.vel_i.assign(vel_i)
+#         self.vel_j.assign(vel_j)
+#         self.dens.assign(dens)
 
-        self.vel_i.assign(induce(vel_i, ctt(self.current[0], dtype=FLOAT), ctt(3, dtype=INT), ctt(1.0, dtype=FLOAT)))
-        self.vel_j.assign(induce(vel_j, ctt(self.current[1], dtype=FLOAT), ctt(3, dtype=INT), ctt(1.0, dtype=FLOAT)))
-        self.dens.assign(induce(dens, ctt(1.0, dtype=FLOAT), ctt(3, dtype=INT), ctt(1.0, dtype=FLOAT)))
+#         # self.vel_i.assign(
+#         #     induce(
+#         #         vel_i,
+#         #         ctt(self.current[0], dtype=FLOAT),
+#         #         ctt(3, dtype=INT),
+#         #         ctt(1.0, dtype=FLOAT),
+#         #     )
+#         # )
+#         # self.vel_j.assign(
+#         #     induce(
+#         #         vel_j,
+#         #         ctt(self.current[1], dtype=FLOAT),
+#         #         ctt(3, dtype=INT),
+#         #         ctt(1.0, dtype=FLOAT),
+#         #     )
+#         # )
+#         # self.dens.assign(
+#         #     induce(
+#         #         dens, ctt(1.0, dtype=FLOAT), ctt(3, dtype=INT), ctt(1.0, dtype=FLOAT)
+#         #     )
+#         # )
 
-    def draw(self, surface: pg.Surface, num_arrows: int = 25) -> None:
-        surface.fill([255, 255, 255])
-        surf_arr = pg.surfarray.pixels3d(surface)
-        surf_arr_scaled = np_grid(surf_arr, *self.shape)
-        dens = self.dens.numpy()
-        colors = (255 - 70 * dens[:, :, None, None, None]).astype(int)
-        colors[colors < 0] = 0
-        colors[colors > 255] = 255
-        surf_arr_scaled[..., :-1] = colors
+#     def draw(self, surface: pg.Surface, num_arrows: int = 25) -> None:
+#         surface.fill([255, 255, 255])
+#         surf_arr = pg.surfarray.pixels3d(surface)
+#         surf_arr_scaled = np_grid(surf_arr, *self.shape)
+#         dens = self.dens.numpy()
+#         colors = (255 - 70 * dens[:, :, None, None, None]).astype(int)
+#         colors[colors < 0] = 0
+#         colors[colors > 255] = 255
+#         surf_arr_scaled[..., :-1] = colors
 
-        vel_scaled = np.mean(
-            np_grid(
-                np.stack([self.vel_i.numpy(), self.vel_j.numpy()], axis=2)
-                * self.dens.numpy()[:, :, None],
-                num_arrows,
-                num_arrows,
-            ),
-            axis=(2, 3),
-        )
-        draw_vec_field(surface, vel_scaled, 0.5, [255, 0, 0])
+#         vel_scaled = np.mean(
+#             np_grid(
+#                 np.stack([self.vel_i.numpy(), self.vel_j.numpy()], axis=2)
+#                 * self.dens.numpy()[:, :, None],
+#                 num_arrows,
+#                 num_arrows,
+#             ),
+#             axis=(2, 3),
+#         )
+#         draw_vec_field(surface, vel_scaled, 0.5, [255, 0, 0])
 
-    # "move" this field to be centered around position
-    # pos is in PM-coordinates
-    def shift(self, i: int, j: int) -> None:
-        i = ctt(i, dtype=INT)
-        j = ctt(j, dtype=INT)
+#     # "move" this field to be centered around position
+#     # pos is in PM-coordinates
+#     def shift(self, i: int, j: int) -> None:
+#         i = ctt(i, dtype=INT)
+#         j = ctt(j, dtype=INT)
 
-        self.vel_i.assign(shift(self.vel_i, i, j, ctt(self.current.x, dtype=FLOAT)))
-        self.vel_j.assign(shift(self.vel_j, i, j, ctt(self.current.y, dtype=FLOAT)))
-        self.dens.assign(shift(self.dens, i, j, ctt(1.0, dtype=FLOAT)))
+#         self.vel_i.assign(shift(self.vel_i, i, j, ctt(self.current.x, dtype=FLOAT)))
+#         self.vel_j.assign(shift(self.vel_j, i, j, ctt(self.current.y, dtype=FLOAT)))
+#         self.dens.assign(shift(self.dens, i, j, ctt(1.0, dtype=FLOAT)))
 
-        self.loc = self.loc[0] + i, self.loc[1] + j
+#         self.loc = self.loc[0] + i, self.loc[1] + j
 
-    def pm_to_grid(self, pos: pm.Vec2d) -> pm.Vec2d:
-        return (pos - self.loc) / self.scale
+#     def pm_to_grid(self, pos: pm.Vec2d) -> pm.Vec2d:
+#         return (pos - self.loc) / self.scale
 
-    def pm_to_idx(self, pos: pm.Vec2d) -> tuple[float, float]:
-        return util.to_int(self.pm_to_grid(pos))
+#     def pm_to_idx(self, pos: pm.Vec2d) -> tuple[float, float]:
+#         return util.to_int(self.pm_to_grid(pos))
 
-    def grid_to_pm(self, pos: pm.Vec2d) -> pm.Vec2d:
-        return pos * self.scale + self.loc
+#     def grid_to_pm(self, pos: pm.Vec2d) -> pm.Vec2d:
+#         return pos * self.scale + self.loc
 
-    def apply_forces(self, obj: pm.Body, shape: pm.Shape, weight: float = 1.0) -> None:
-        rr, cc = shape_to_rrcc(obj, shape, self.scale, self.loc)
-        (vel_i, vel_j), ((force_i, force_j), torque) = get_forces(
-            self.vel_i,
-            self.vel_j,
-            self.dens,
-            ctt(obj.position.x, dtype=FLOAT),
-            ctt(obj.position.y, dtype=FLOAT),
-            ctt(obj.velocity.x, dtype=FLOAT),
-            ctt(obj.velocity.y, dtype=FLOAT),
-            ctt(obj.angular_velocity, dtype=FLOAT),
-            ctt(weight, dtype=FLOAT),
-            rr,
-            cc,
-            ctt(self.scale, dtype=FLOAT),
-            ctt(self.loc[0], dtype=FLOAT),
-            ctt(self.loc[1], dtype=FLOAT),
-        )
-        force = pm.Vec2d(force_i.numpy(), force_j.numpy())
-        print(force, torque.numpy())
-        obj.apply_force_at_world_point(force, obj.center_of_gravity)
-        obj.torque += torque.numpy()
-        self.vel_i.assign(vel_i)
-        self.vel_j.assign(vel_j)
-
+#     def apply_forces(self, obj: pm.Body, shape: pm.Shape, weight: float = 1.0) -> None:
+#         rr, cc = shape_to_rrcc(obj, shape, self.scale, self.loc)
+#         (vel_i, vel_j), ((force_i, force_j), torque) = get_forces(
+#             self.vel_i,
+#             self.vel_j,
+#             self.dens,
+#             ctt(obj.position.x, dtype=FLOAT),
+#             ctt(obj.position.y, dtype=FLOAT),
+#             ctt(obj.velocity.x, dtype=FLOAT),
+#             ctt(obj.velocity.y, dtype=FLOAT),
+#             ctt(obj.angular_velocity, dtype=FLOAT),
+#             ctt(weight, dtype=FLOAT),
+#             rr,
+#             cc,
+#             ctt(self.scale, dtype=FLOAT),
+#             ctt(self.loc[0], dtype=FLOAT),
+#             ctt(self.loc[1], dtype=FLOAT),
+#         )
+#         force = pm.Vec2d(force_i.numpy(), force_j.numpy())
+#         print(force, torque.numpy())
+#         obj.apply_force_at_world_point(force, obj.center_of_gravity)
+#         obj.torque += torque.numpy()
+#         self.vel_i.assign(vel_i)
+#         self.vel_j.assign(vel_j)
 
 
 if __name__ == "__main__":
-    space = pm.Space()
-    space.damping = 0.95
-    wing = pm.Body(body_type=pm.Body.DYNAMIC)
+    pass
 
-    wing_shape = pm.Segment(wing, pm.Vec2d(-30, 0), pm.Vec2d(30, 0), 0.1)
+    # space = pm.Space()
+    # space.damping = 0.95
+    # wing = pm.Body(body_type=pm.Body.DYNAMIC)
 
-    wing_shape.density = 10.0
-    wing.position = pm.Vec2d(0, 0)
-    wing.angular_velocity = 1
-    wing.angle = 0.5
+    # wing_shape = pm.Segment(wing, pm.Vec2d(-30, 0), pm.Vec2d(30, 0), 0.1)
 
-    space.add(wing, wing_shape)
+    # wing_shape.density = 10.0
+    # wing.position = pm.Vec2d(0, 0)
+    # wing.angular_velocity = 1
+    # wing.angle = 0.5
 
-    fl = Fluid([500, 500], pm.Vec2d(0.5, 0), (-250, -250), 1.0)
+    # space.add(wing, wing_shape)
 
-    pg.init()
-    surface = pg.display.set_mode((1000, 1000))
+    # fl = Fluid([500, 500], pm.Vec2d(0.5, 0), (-250, -250), 1.0)
 
-    clock = pg.time.Clock()
-    running = True
-    while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
+    # pg.init()
+    # surface = pg.display.set_mode((1000, 1000))
 
-        for _ in range(1):
-            for _ in range(5):
-                space.step(2 / 60)
-                fl.run(1, 2 / 60)
+    # clock = pg.time.Clock()
+    # running = True
+    # while running:
+    #     for event in pg.event.get():
+    #         if event.type == pg.QUIT:
+    #             running = False
 
-                fl.apply_forces(wing, wing_shape, 0.3)
+    #     for _ in range(1):
+    #         for _ in range(5):
+    #             space.step(2 / 60)
+    #             fl.run(1, 2 / 60)
 
-        fl.draw(surface, 40)
+    #             fl.apply_forces(wing, wing_shape, 0.3)
 
-        draw_util.draw_shape(
-            surface, wing, wing_shape, surface.get_width() / fl.shape[0]
-        )
-        pg.display.flip()
+    #     fl.draw(surface, 40)
 
-        # clock.tick(60)  # limits FPS to 60
-    pg.quit()
+    #     draw_util.draw_shape(
+    #         surface, wing, wing_shape, surface.get_width() / fl.shape[0]
+    #     )
+    #     pg.display.flip()
+
+    #     # clock.tick(60)  # limits FPS to 60
+    # pg.quit()
--- readme.md
+++ readme.md
@@ -1,16 +1,13 @@
-### A WIP project for simulating a sailboat using CFD, by LemonKat.
+A WIP project to simulate the movement of a sailboat, with a keel in the water and a sail in the air. The program approximates Navier-Stokes partial differential equations from computational fluid dynamics with 3 grids of float values, storing velocity and density of a fluid. The TensorFlow library is used to optimize everything as much as possible.
 
 ![example screenshot](images/screenshot1.png)
 
-This program approximates the Navier-Stokes equations with 3 grids of float values, storing velocity and density of a fluid. The TensorFlow library is used to optimize everything as much as possible.
 
 Inspired by these papers:
 1. [_The Story of Airplane Wings_](https://arxiv.org/abs/2010.07446)
 2. [_Real-Time Fluid Dynamics for Games_](http://graphics.cs.cmu.edu/nsp/course/15-464/Fall09/papers/StamFluidforGames.pdf)
 
-The sailboat part is a work in progress, but an example of the fluid flowing around an object can be run by running `python3 fluid.py`.
-
-#### Required libraries:
+Required libraries:
 1. `numpy`
 2. `pygame`
 3. `pymunk`
